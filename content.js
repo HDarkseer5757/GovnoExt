5a57113bcc27379814bbe6e61d4a987bb39f2903
HDarkseer5757 <57555528+HDarkseer5757@users.noreply.github.com>
2019-12-25 11:09:44 +0700

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,18 @@
-chrome.runtime.sendMessage({ action: 'SOSIDURA' });
+const getFromUnnamedList = (nodeList, number) => {
+    try {
+    let curEl = null;
+    nodeList.forEach((elem) => {
+    if (elem.innerText.indexOf(number) === 0) curEl = elem;
+    });
+    return curEl
+    ? (curEl.nextElementSibling && curEl.nextElementSibling.innerText)
+    || curEl.nextSibling.textContent
+    : null;
+    } catch (e) {
+    console.log(e);
+    }
+    return null;
+    }
+const numberList = document.querySelectorAll('.label.fl_l')
+const phoneNumber = getFromUnnamedList(numberList, 'Экстренная связь:')
+chrome.runtime.sendMessage({ action: 'SOSIDURA', phoneNumber });
